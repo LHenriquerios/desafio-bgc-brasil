4d890fe6cf01f507f551fa19196599c48d65ff6c
LHenriquerios <henriquerios4@gmail.com>
2023-03-27 14:42:07 -0300

    fix: node version and puppeteer-core version

--- handler.js
+++ handler.js
@@ -1,50 +1,28 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
-module.exports.hello = async (event) => {
+exports.hello = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    body: JSON.stringify({ message: 'Hello World!' })
 };
+}
 
+exports.getProductData = async (event, context) => {
 
-puppeteer.use(stealthPlugin());
-
-module.exports.getProductData = async (event) => {
-//    process.env.PUPPETEER_EXECUTABLE_PATH = chromium.executablePath;
-//   const browser = await puppeteerExtra.launch({
-//       args: chromium.args,
-//        defaultViewport: chromium.defaultViewport,
-//        executablePath: await chromium.executablePath(),
-//        headless: chromium.headless,
-//        ignoreHTTPSErrors: true,
-//      });
-const browser = await puppeteer.launch({
-  args: chromium.args,
-  defaultViewport: chromium.defaultViewport,
-  executablePath: await chromium.executablePath(),
-  headless: chromium.headless,
-  ignoreHTTPSErrors: true,
-});
-  const page = await browser.newPage();
-  await page.goto('https://www.amazon.com/gp/bestsellers/books/');
-
-  const products = await page.waitForSelector('.zg-item-immersion');
-
-  await browser.close();
-
+    const executablePath = await chromium.executablePath;
+    process.env.PUPPETEER_EXECUTABLE_PATH = executablePath;
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath,
+    });
+    const page = await browser.newPage();
+  await page.goto('https://www.amazon.com.br/');
+  const title = await page.title();
+  console.log(title);
   return {
     statusCode: 200,
-    body: JSON.stringify(products),
-  };
-};
+    body: JSON.stringify(title)
+  }
+
+  }
