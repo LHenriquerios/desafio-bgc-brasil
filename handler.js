7adc187de9520f3f3cd2f380a4761c2bdfcdcc80
LHenriquerios <henriquerios4@gmail.com>
2023-03-27 20:23:11 -0300

    feat: get list of three products

--- handler.js
+++ handler.js
@@ -9,20 +9,36 @@ exports.hello = async (event) => {
 }
 
 exports.getProductData = async (event, context) => {
+  const executablePath = await chromium.executablePath;
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath,
+  });
+  const page = await browser.newPage();
 
-    const executablePath = await chromium.executablePath;
-    process.env.PUPPETEER_EXECUTABLE_PATH = executablePath;
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath,
-    });
-    const page = await browser.newPage();
-  await page.goto('https://www.amazon.com.br/');
-  const title = await page.title();
-  console.log(title);
+  // Navegar até a página "Mais Vendidos" da Amazon
+  await page.goto('https://www.amazon.com.br/bestsellers/');
+
+  // Obter os três primeiros produtos da lista
+  const pageData = await page.evaluate(() => {
+  const products = [];
+  
+  for (let i = 0; i < 3; i++ ) {
+    const product = {};
+    product.title =  document.querySelectorAll('.p13n-sc-truncate-desktop-type2')[i].innerText;
+    product.price = document.querySelectorAll('.a-size-base.a-color-price')[i].innerText;
+    product.link =  document.querySelectorAll('.a-link-normal')[i+1].href;
+    
+    products.push(product);
+    };
+  
+    return products;
+  });
+
+  await browser.close();
+
   return {
     statusCode: 200,
-    body: JSON.stringify(title)
-  }
-
-  }
+    body: JSON.stringify(pageData)
+  };
+}
